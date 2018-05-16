614067b85721939e0de20e3e5d1a9d70df85e908
MadLicorice <34589724+MadLicorice@users.noreply.github.com>
2018-05-16 14:33:19 -0700

    Finish 821 optimized

--- easy/821-shortestDistanceToACharacter/shortestDistanceToACharacter.js
+++ easy/821-shortestDistanceToACharacter/shortestDistanceToACharacter.js
@@ -11,33 +11,38 @@ All letters in S and C are lowercase.
 */
 
 var shortestToChar = function (S, C) {
-  let result = [];
-  let locationsOfC = [];
+  const result = [];
+  const locationsOfC = [];
   let currentIndexOfC = 0;
   let i;
-  
+
   for (i = 0; i < S.length; i += 1) {
     if (S[i] === C) {
       locationsOfC.push(i)
     }
   }
-  
-  for (i = 0; i < S.length; i += 1) {
-    if (currentIndexOfC === i) {
-      result.push(0);
-      currentIndexOfC += 1;
-    } else {
-      let firstDiff = currentIndexOfC - i;
-      let secondDiff = currentIndexOfC - (i + 1);
-      if (firstDiff < 0) {
-        firstDiff *= -1;
-      }
-      if (firstDiff > secondDiff) {
+
+  if (locationsOfC.length > 1) {
+    for (i = 0; i < S.length; i += 1) {
+      let firstDiff = Math.abs(locationsOfC[currentIndexOfC] - i);
+      let secondDiff = Math.abs(locationsOfC[currentIndexOfC + 1] - i);
+      if (firstDiff < secondDiff) {
         result.push(firstDiff);
       } else {
         result.push(secondDiff);
-        currentIndexOfC += 1;
+        if ((currentIndexOfC + 2) !== locationsOfC.length) {
+          currentIndexOfC += 1;
+        }
       }
     }
+  } else if (locationsOfC.length === 1) {
+    for (i = 0; i < S.length; i += 1) {
+      let diff = Math.abs(locationsOfC[currentIndexOfC] - i);
+      result.push(diff);
+    }
+  } else {
+    return result;
   }
+
+  return result;
 };
